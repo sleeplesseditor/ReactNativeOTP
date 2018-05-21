c79f82572868ab28d468e6ec264b945c96d934c8
sleeplesseditor <josh@bryson.me.uk>
2018-05-21 19:26:14 +0100

    serviceAccount setup

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
+const serviceAccount = require('../service_account.json')
 
+//Insert your own service account data from Firebase created JSON file.
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://ot-password.firebaseio.com"
