65cab6a6fe86cc310ecb450e6f118065bea67ebc
sleeplesseditor <josh@bryson.me.uk>
2018-05-21 19:22:12 +0100

    Admin and createUser function setup

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+const createUser = require('./create_user');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://ot-password.firebaseio.com"
+});
+
+exports.createUser = functions.https.onRequest(createUser);
